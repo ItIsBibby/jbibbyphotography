fee7ceb9847aa9c176ba954559ca7d58797aeb44
Joe Bibby <joseph_bibby@hotmail.co.uk>
2023-03-02 23:48:39 +0000

    gallery index added

---
+++ src/img/index.js
@@ -0,0 +1,25 @@
+import photo1 from '. photo1.png';
+import photo2 from '. photo2.png';
+import photo3 from '. photo3.png';
+import photo4 from '. photo4.png';
+import photo5 from '. photo5.png';
+import photo6 from '. photo6.png';
+import photo7 from '. photo7.png';
+import photo8 from '. photo8.png';
+import photo9 from '. photo9.png';
+import photo10 from '. photo10.png';
+import photo11 from '. photo11.png';
+
+export {
+ photo1,
+ photo2,
+ photo3,
+ photo4,
+ photo5,
+ photo6,
+ photo7,
+ photo8,
+ photo9,
+ photo10,
+ photo11
+};
